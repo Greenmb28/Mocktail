532b1a956e69a01e1b959d967cc182a503c3ee38
Greenmb28 <greenmb@purdue.edu>
2025-04-06 13:41:44 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const boxesSet = () => 
+{
+    let wrapperRef = $('.wrapper');
+    mocktails.forEach((mocktail)=>{
+        let boxMarkup= ` <div class="box">
+         <img class="mocktail_pic" src="./images/${mocktail.drink.toLowerCase()}.png" />
+        <p class="drink"></p>
+      </div>`;
+      wrapperRef.append(boxMarkup);
+    }
+    );
+}
+
+const showInfo = () =>
+{
+    boxesSet();
+    jQuery.each($(".box"), drinksInfo);
+    $(".box").on("click", function () 
+    {
+        let indexDrinks = $(".box").index(this);
+        $(this).find("p.drink").text(mocktails[indexDrinks].calories+ " Calories " );
+    });
+    $(".box").on("mouseover", function () {
+        let indexDrinks = $(".box").index(this);
+        $(this).find("p.drink").text(mocktails[indexDrinks].ingrediants 
+          );
+    });
+    $(".box").on("mouseout", function () {
+        let indexDrinks = $(".box").index(this);
+    
+        $(this).find("p.drink").text(mocktails[indexDrinks].drink);
+        
+      });
+    };
+    const drinksInfo = (index) => {
+        console.log(mocktails[index].drink);
+        $(".drink").eq(index).text(mocktails[index].drink);
+    };
+    $(document).ready(showInfo);
---
+++ mocktail_info.js
@@ -0,0 +1,45 @@
+const mocktails = 
+[
+    {
+        drink:"Strawberry Daiquiri",
+        calories:110,
+        ingrediants:"Frozen Strawberries,White Sugar, Lemon, Lime, Rum, Citrus Soda",
+
+    },
+    {
+        drink:"Honey Blackberry Mint",
+        calories:346,
+        ingrediants:"Frozen Blackberries, Honey, Mint, Lemon, Water, Seltzer",
+        
+    },
+    {
+        drink:"Shirley Temple",
+        calories:97,
+        ingrediants:"Lemonlime Soda, Grenadine syrup, Ice, Maraschino cherries",
+        
+    },
+    {
+        drink:"Raspberry Lemonade Mojito",
+        calories:310,
+        ingrediants:"Mint, Raspberries, Lemon, Bacardi, Raspberry lime seltzer, raspberry syrup",
+        
+    },
+    {
+        drink:"Rum Punch",
+        calories:231,
+        ingrediants:"Rum, Pinapple juice, Orange juice, Lime, Grenadine ",
+        
+    },
+    {
+        drink:"S'more Martini",
+        calories:271,
+        ingrediants:"Chocolate syrup, Graham crackers, Chocolate liqueur, Marshmellow vodka, Heavy cream, Marshmallow",
+        
+    },
+    {
+        drink:"Pina Colada",
+        calories:230,
+        ingrediants:"Coconut, Run, Pinapple, Lime, Ice",
+        
+    }
+];
