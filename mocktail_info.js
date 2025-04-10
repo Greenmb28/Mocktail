a945e1e1e7ee976e52e6c384977aaed1ebef1991
Greenmb28 <greenmb@purdue.edu>
2025-04-10 10:54:06 -0400

    Update mocktail_info.js

--- mocktail_info.js
+++ mocktail_info.js
@@ -1,43 +1,43 @@
 const mocktails = 
 [
     {
-        drink:"Strawberry Daiquiri",
+        drink:"strawberry daiquiri",
         calories:110,
         ingrediants:"Frozen Strawberries,White Sugar, Lemon, Lime, Rum, Citrus Soda",
 
     },
     {
-        drink:"Honey Blackberry Mint",
+        drink:"honey blackberry mint",
         calories:346,
         ingrediants:"Frozen Blackberries, Honey, Mint, Lemon, Water, Seltzer",
         
     },
     {
-        drink:"Shirley Temple",
+        drink:"shirley temple",
         calories:97,
         ingrediants:"Lemonlime Soda, Grenadine syrup, Ice, Maraschino cherries",
         
     },
     {
-        drink:"Raspberry Lemonade Mojito",
+        drink:"raspberry lemonade mojito",
         calories:310,
         ingrediants:"Mint, Raspberries, Lemon, Bacardi, Raspberry lime seltzer, raspberry syrup",
         
     },
     {
-        drink:"Rum Punch",
+        drink:"rum punch",
         calories:231,
         ingrediants:"Rum, Pinapple juice, Orange juice, Lime, Grenadine ",
         
     },
     {
-        drink:"S'more Martini",
+        drink:"smore martini",
         calories:271,
         ingrediants:"Chocolate syrup, Graham crackers, Chocolate liqueur, Marshmellow vodka, Heavy cream, Marshmallow",
         
     },
     {
-        drink:"Pina Colada",
+        drink:"pina colada",
         calories:230,
         ingrediants:"Coconut, Run, Pinapple, Lime, Ice",
         
